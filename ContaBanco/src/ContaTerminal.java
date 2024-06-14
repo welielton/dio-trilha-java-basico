273b6be4cf987a3a8ee6ecbefa4cba61cff8f389
welielton <welielton.silva@hotmail.com>
2024-06-14 17:54:43 -0300

    Desafio concluído de simular a criação de uma conta.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,25 +1,24 @@
+   import java.util.Locale;
+   import java.util.Scanner;
 public class ContaTerminal {
+
+    
     public static void main(String[] args) {
-        //TODO:Conhecer e importar a classe Scanner
+        System.out.println("Criar conta no banco DIO");
 
-        //Exibir as mensagens para o nosso usuário
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da sua conta:");
+        int numero = scanner.nextInt();
 
-        //Exibir a mensagem conta criada
-        
-        /*
-         * Numero	Inteiro	1021
-         * Agencia	Texto	067-8
-         * Nome Cliente	Texto	MARIO ANDRADE
-         * Saldo	Decimal	237.48
-         * 
-         */
-        
-         int numero = Integer.valueOf(args[0]);
-         String agencia = args[1];
-         String nome = args[2];
-         double saldo = Double.valueOf(args[3]);
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Agora, você deve informar o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("E para finalizar, deseje o saldo que será depositado: ");
+        double saldo = scanner.nextDouble();        
 
          System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: "
           + agencia + " conta: " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
